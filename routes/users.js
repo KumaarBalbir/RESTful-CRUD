8ac4924a9164f451e4e3f8bac00f4457ba9583c9
Kumaar-Balbir <prasadbalbir1056@gmail.com>
2024-03-29 16:14:07 +0530

    adds users details

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,27 @@ const express = require("express");
 
 const router = express.Router();
 
+const users = [
+  {
+    firstName: "Balbir",
+    lastName: "Singh",
+    age: 23,
+  },
+  {
+    firstName: "Pranav",
+    lastName: "Sha",
+    age: 21,
+  },
+  {
+    firstName: "Rohit",
+    lastName: "Prasad",
+    age: 25,
+  },
+];
+
 // all routes here will start with /users
 router.get("/", (req, res) => {
-  res.send("users");
+  res.send(users);
 });
 
 module.exports = router;
